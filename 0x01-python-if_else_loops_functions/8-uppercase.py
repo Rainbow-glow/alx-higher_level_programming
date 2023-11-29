12afa3b97bcad7e3bcdcfd4a060af061ef11390f
Rainbow-glow <emmanuellanweze97@gmail.com>
2023-11-28 17:06:47 -0800

    P

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@
 def uppercase(str):
     """prints a string in uppercase followed by a new line."""
     for i in str:
-        if ord("a") <= ord(i) <= ord("z"):
+        if ord("A") <= ord(i) <= ord("Z"):
             i = chr(ord(i) - 32)
         print("{:s}".format(i), end="")
     print()
