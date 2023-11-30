edc185bccd6e3714c8f80695a4ddf043831d04b0
Rainbow-glow <emmanuellanweze97@gmail.com>
2023-11-30 15:34:10 -0800

    modules

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__=="__main__":
+    import sys
+
+    count = 0
+    for i in range(len(sys.argv) - 1):
+        count = count + int(sys.argv[i + 1])
+    print("{}".format(count))
