3986586590c12a6d7b6cba4d7736124b33aa738b
Rainbow-glow <emmanuellanweze97@gmail.com>
2023-11-30 14:41:39 -0800

    modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    num = len(sys.argv) - 1
+    if num == 0:
+        print("0 argument.")
+    elif num == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num))
+    for a in range(num):
+        print("{}: {}".format(a + 1, sys.argv[a + 1]))
