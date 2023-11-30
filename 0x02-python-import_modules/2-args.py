77955c0f63e51217b340040178483292bee39cf6
Rainbow-glow <emmanuellanweze97@gmail.com>
2023-11-30 15:42:45 -0800

    modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 
     num = len(sys.argv) - 1
     if num == 0:
-        print("0 argument.")
+        print("0 arguments.")
     elif num == 1:
         print("1 argument:")
     else:
