d5b4c799ee5e286adb48afca34b85688636d3a5c
Rainbow-glow <emmanuellanweze97@gmail.com>
2023-11-30 16:26:17 -0800

    modules

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        c = add(a, b)
+        for r in range(4, 6);
+            c = add(c, r)
+        return (c)
+
+    else:
+        return(sub(a, b))
