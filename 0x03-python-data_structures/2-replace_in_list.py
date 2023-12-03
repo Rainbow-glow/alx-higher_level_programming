0a074c24d6bdf254e5527293a67d3b57873c94fc
Rainbow-glow <emmanuellanweze97@gmail.com>
2023-12-03 13:31:24 -0800

    list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    if idx >= 0:
+        my_list[idx] = element
+    elif idx < len(my_list):
+        my_list[idx] = element
+    return (my_list)
