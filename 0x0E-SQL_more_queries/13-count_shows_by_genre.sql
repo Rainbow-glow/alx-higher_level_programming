2f7d0a928e9e4b6332011d80d2e5e599be518c65
Rainbow-glow <emmanuellanweze97@gmail.com>
2024-02-14 11:49:28 -0800

    fixing errors

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,7 +3,7 @@
 -- Does not display genres without linked shows.
 -- Records are ordered by descending number of shows linked.
 SELECT j.`name` AS `genre`,
-       NUM(*) AS `number_of_shows`
+       COUNT(*) AS `number_of_shows`
   FROM `tv_genres` AS j
        INNER JOIN `tv_show_genres` AS w
        ON j.`id` = w.`genre_id`
