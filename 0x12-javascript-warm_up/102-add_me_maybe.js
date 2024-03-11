37ed852e050e983c76019140782150a8a505000f
Rainbow-glow <emmanuellanweze97@gmail.com>
2024-03-11 13:03:31 -0700

    uploading advanced tasks

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+exports.addMeMaybe = function (number, theFunction) {
+  theFunction(++number);
+};#!/usr/bin/node
+exports.addMeMaybe = function (number, theFunction) {
+	  theFunction(++number);
+};
